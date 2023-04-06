cfee17878341ceeb2161f82bd308ddad852a1318
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-04-06 13:13:14 -0400

    passes tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link) => {
+    let localLink = `#${link}`
+    return <a key={link} href={localLink}>{link}</a>
+  })
+
+  return <nav>{ linkElements }</nav>;
 }
 
 export default NavBar;
